e50cf314a72c250bb75d4416046c5a72a03c6461
Arael D. Espinosa <cl8dep@gmail.com>
2021-11-23 11:26:02 -0500

    Refactorization of home styles to emotion

--- src/modules/home/views/Home.tsx
+++ src/modules/home/views/Home.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Typography, AppBar, Box, Toolbar, Button, Grid } from '@mui/material';
 
-import { ReactComponent as LandingBg } from '~/assets/vectors/landingbg.svg';
-import useStyles from './styles';
+import {
+  ButtonContainer,
+  Description,
+  RootContainer,
+  SvgImage,
+  TextContainer,
+  Title,
+} from './styles';
 
 function Home(): JSX.Element {
   const [t] = useTranslation();
-  const classes = useStyles();
 
   return (
     <>
@@ -21,34 +25,18 @@ function Home(): JSX.Element {
           </Button> */}
         </Toolbar>
       </AppBar>
-      <Grid container className={classes.rootContainer}>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          md={7}
-          xl={7}
-          className={classes.textContainer}
-        >
-          <Typography
-            variant={'h3'}
-            fontWeight={500}
-            letterSpacing={0.72}
-            lineHeight={1.2}
-          >
-            {t('home.title')}
-          </Typography>
-          <Typography variant={'h6'}>{t('home.subtitle')}</Typography>
-          <Box className={classes.buttonContainer}>
-            <Button variant="contained" className={classes.startButton}>
-              {t('home.login')}
-            </Button>
-          </Box>
-        </Grid>
+      <RootContainer container>
+        <TextContainer item xs={12} sm={12} md={7} xl={7}>
+          <Title variant={'h3'}>{t('home.title')}</Title>
+          <Description variant={'h6'}>{t('home.subtitle')}</Description>
+          <ButtonContainer>
+            <Button variant="contained">{t('home.login')}</Button>
+          </ButtonContainer>
+        </TextContainer>
         <Grid item xs={12} sm={12} md={5} xl={5}>
-          <LandingBg className={classes.svgImage} />
+          <SvgImage />
         </Grid>
-      </Grid>
+      </RootContainer>
     </>
   );
 }
--- src/modules/home/views/styles.ts
+++ src/modules/home/views/styles.ts
@@ -1,27 +1,64 @@
+import { Grid, styled, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
-const useStyles = makeStyles(() => ({
-  rootContainer: {
-    display: 'flex',
-    padding: '0 3em 2em 6em',
-    overflowX: 'hidden',
-  },
-  textContainer: {
-    paddingTop: '8em',
-  },
-  svgImage: {
-    marginTop: 20,
-    marginRight: -150,
-    height: 530,
-  },
-  buttonContainer: {
-    display: 'flex',
-    margin: '30px 30px 30px 0',
-    maxWidth: '80%',
-  },
-  startButton: {
-    width: '16em',
+import { ReactComponent as LandingBg } from '~/assets/vectors/landingbg.svg';
+
+export const RootContainer = styled(Grid)(({ theme }) => ({
+  display: 'flex',
+  padding: '0 3em 0 6em',
+  position: 'relative',
+  overflowX: 'hidden',
+  [theme.breakpoints.down('md')]: {
+    padding: '1em 1em 1em 1em',
+    height: '100vh',
   },
 }));
 
-export default useStyles;
+export const TextContainer = styled(Grid)(({ theme }) => ({
+  paddingTop: '8em',
+
+  [theme.breakpoints.down('md')]: {
+    paddingTop: '1em',
+  },
+}));
+
+export const Title = styled(Typography)(({ theme }) => ({
+  lineHeight: 1.2,
+  letterSpacing: 0.72,
+  fontWeight: 600,
+
+  [theme.breakpoints.down('md')]: {
+    textAlign: 'center',
+    fontSize: '2rem',
+  },
+}));
+
+export const Description = styled(Typography)(({ theme }) => ({
+  marginTop: '7px',
+  [theme.breakpoints.down('md')]: {
+    textAlign: 'center',
+    fontSize: '1.2rem',
+  },
+}));
+
+export const ButtonContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  margin: '30px 30px 30px 0',
+
+  [theme.breakpoints.down('md')]: {
+    justifyContent: 'center',
+  },
+}));
+
+export const SvgImage = styled(LandingBg)(({ theme }) => ({
+  marginTop: 20,
+  marginRight: -150,
+  height: 530,
+
+  [theme.breakpoints.down('md')]: {
+    position: 'absolute',
+    top: 0,
+    opacity: 0.07,
+    height: '100vh',
+  },
+}));
