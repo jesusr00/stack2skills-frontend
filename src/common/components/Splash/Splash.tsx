143eb348a58d859fd6679f8400fb06254f031bb6
jesusrlm <jesus.reikel@gmail.com>
2021-11-26 14:55:51 -0500

    implemet lotties anim

--- src/common/components/Splash/Splash.tsx
+++ src/common/components/Splash/Splash.tsx
@@ -1,5 +1,15 @@
+import Lottie from 'react-lottie-player';
+import loadingSplashAnim from '~/assets/lotties/loading_splash_anim.json';
+
 function Splash() {
-  return <div>Loading...</div>;
+  return (
+    <Lottie
+      loop
+      animationData={loadingSplashAnim}
+      play
+      style={{ width: 150, height: 150 }}
+    />
+  );
 }
 
 export default Splash;
